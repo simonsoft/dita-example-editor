e84ed6ce6a9907a89fcf3c923069def5c1216e3b
Thomas Åkesson <thomas.akesson@simonsoft.se>
2025-05-13 10:31:40 +0200

    Numbering working of bookmap.

---
+++ packages/automatic-numbering/src/NumberingFunctions.xqm
@@ -0,0 +1,18 @@
+module namespace app = "http://example.app/ns";
+
+declare %public function app:divisionNumberingCallback(
+    $previousAccumulator as item()*,
+    $relType as xs:string,
+    $node as node(),
+    $isUnloaded as xs:boolean
+) as item()* {
+    if ($relType eq "first") then
+	    (: Return 1 for the first element, $previousAccumulator does not contain a value yet :)
+	    1
+	else if ($relType eq "parent") then
+	    (: Add a new layer of "indentation" to the accumulator sequence :)
+	    ($previousAccumulator, 1)
+	else
+	    (: Increment the last item in the accumulator sequence :)
+	    $previousAccumulator ! (if (position() = last()) then . + 1 else .)
+};
---
+++ packages/automatic-numbering/src/configureSxModule.ts
@@ -0,0 +1,57 @@
+import configureAsStructureViewItem from 'fontoxml-families/src/configureAsStructureViewItem';
+import addReducer from 'fontoxml-indices/src/addReducer';
+import xq from 'fontoxml-selectors/src/xq';
+
+export default function configureSxModule(sxModule) {
+	sxModule.markAsAddon();
+
+	addReducer(
+		// namespace and name of the reducer function being defined
+		'http://example.app/ns',
+		'getDivisionNumber',
+		// selector matching nodes that should be counted
+		"self::*[name()=('chapter', 'division', 'divisions')][not(@format='ditamap')]",
+		// namespace and name of the accumulator function we defined above
+		'http://example.app/ns',
+		'divisionNumberingCallback'
+	);
+
+	addReducer(
+		// namespace and name of the reducer function being defined
+		'http://example.app/ns',
+		'getAppendixNumber',
+		// selector matching nodes that should be counted
+		"self::*[name()=('appendix', 'appendices')][not(@format='ditamap')]",
+		// namespace and name of the accumulator function we defined above
+		'http://example.app/ns',
+		'divisionNumberingCallback'
+	);
+
+
+
+	configureAsStructureViewItem(sxModule, xq`self::*[fonto:dita-class(., 'topic/topic')]`, {
+		titleQuery: xq`
+		import module namespace app = "http://example.app/ns";
+		import module namespace fonto = "http://www.fontoxml.com/functions";
+
+		let $numAppendix := app:getAppendixNumber(fonto:current-hierarchy-node-id(), .)
+		let $numbering := 
+			if (exists(app:getDivisionNumber(fonto:current-hierarchy-node-id(), .))) then
+				app:getDivisionNumber(fonto:current-hierarchy-node-id(), .)
+			else if (exists($numAppendix)) then
+				(: Convert first level to alpha-upper. :)
+				$numAppendix ! (if (position() = 1) then fn:format-integer(xs:integer(.), 'A') else .)
+			else ()
+
+		return if (not(empty($numbering))) then ( 
+			string-join($numbering,	'.') || ' - ' || fonto:curated-text-in-node(.) 
+		) else (
+			(: If numbering function not applicable to the element :)
+			fonto:curated-text-in-node(.) 
+		)
+		`,
+		icon: 'file-text-o',
+		isDraggable: true,
+		priority: 1000
+	});
+}
